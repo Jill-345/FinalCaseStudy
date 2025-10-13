c25fa8a4e5ad8bb8636515e27a1707a7e4fd45a9
jenilynlabbao858-star <jenilynlabbao858@gmail.com>
2025-10-13 14:04:00 +0800

    spinner

--- app/src/main/java/com/example/finalcasestudy/ItemFoundFrame.java
+++ app/src/main/java/com/example/finalcasestudy/ItemFoundFrame.java
@@ -1,6 +1,11 @@
 package com.example.finalcasestudy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
